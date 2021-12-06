006243330ab1f1b6dc0cfd9033d48b664ca9c923
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-06 16:51:03 +0000

    Added SelectPaste component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { PastesDisplay } from "./components/PastesDisplay";
+import { SelectedPaste } from "./components/SelectedPaste";
+import { IPaste } from "./utils/IPaste";
 
 function App(): JSX.Element {
+  const [selection, setSelection] = useState<IPaste | undefined>();
+
   return (
     <>
-      <PastesDisplay />
+      <PastesDisplay setSelection={setSelection} />
+      <h1>==========================================</h1>
+      <SelectedPaste selection={selection} />
     </>
   );
 }
