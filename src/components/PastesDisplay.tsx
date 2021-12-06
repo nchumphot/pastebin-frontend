528fdc6a5b6b0678e3d154fa5204cf30d19bfa00
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-06 16:51:21 +0000

    Modified the date format

--- src/components/PastesDisplay.tsx
+++ src/components/PastesDisplay.tsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "../utils/fetchData";
 import { IPaste } from "../utils/IPaste";
+import { ISO8601toDate } from "../utils/ISO8601toDate";
 
-export function PastesDisplay(): JSX.Element {
+export function PastesDisplay(props: {
+  setSelection: React.Dispatch<React.SetStateAction<IPaste | undefined>>;
+}): JSX.Element {
   const [pastes, setPastes] = useState<IPaste[]>([]);
 
   useEffect(() => {
     fetchData("https://pastebin-c3a8.herokuapp.com/pastes/recent", setPastes);
   }, []);
 
-  console.log(pastes);
-
   const PasteItems = (item: IPaste): JSX.Element => {
+    const dateStr = ISO8601toDate(item.creation_date);
     return (
-      <div className="paste">
+      <div className="paste" key={item.id}>
         {item.title === null ? <h3>Untitled</h3> : <h3>{item.title}</h3>}
-        <small>{item.creation_date}</small>
+        <small>{dateStr}</small>
         <p>{item.body}</p>
+        <button onClick={() => props.setSelection(item)}>See more</button>
       </div>
     );
   };
