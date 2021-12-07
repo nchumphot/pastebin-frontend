57dda81ff1aa3c4267608436dc9ba1c221a4a3ed
Hanna <sophianhanna@gmail.com>
2021-12-07 10:17:30 +0000

    Moved pastes state to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import { PastesDisplay } from "./components/PastesDisplay";
+import { PostModal } from "./components/PostModal";
 import { SelectedPaste } from "./components/SelectedPaste";
 import { IPaste } from "./utils/IPaste";
+import { useEffect } from "react";
+import { fetchData } from "./utils/fetchData";
 
 function App(): JSX.Element {
   const [selection, setSelection] = useState<IPaste | undefined>();
+  const [pastes, setPastes] = useState<IPaste[]>([]);
+
+  useEffect(() => {
+    fetchData("https://pastebin-c3a8.herokuapp.com/pastes/recent", setPastes);
+  }, [pastes]);
 
   return (
     <>
-      <PastesDisplay setSelection={setSelection} />
+      <PostModal {...{ pastes, setPastes }} />
+      <PastesDisplay setSelection={setSelection} {...{ pastes, setPastes }} />
       <h1>==========================================</h1>
       <SelectedPaste selection={selection} />
     </>
