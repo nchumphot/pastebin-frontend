972cc2fefc2496850414042cab9ceecbe8e3e642
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-09 15:39:02 +0000

    Added class names

--- src/components/SelectedPaste.tsx
+++ src/components/SelectedPaste.tsx
@@ -7,21 +7,23 @@ export function SelectedPaste(props: {
 }): JSX.Element {
   if (props.selection !== undefined) {
     return (
-      <div className="selectedpaste">
+      <div className="selected-paste">
         {props.selection.title === null ? (
-          <h3>Untitled</h3>
+          <h3 className="selected-paste-title">Untitled</h3>
         ) : (
-          <h3>{props.selection.title}</h3>
+          <h3 className="selected-paste-title">{props.selection.title}</h3>
         )}
-        <small>{ISO8601toDate(props.selection.creation_date)}</small>
+        <small className="selected-paste-date">
+          {ISO8601toDate(props.selection.creation_date)}
+        </small>
         <LineBreak str={props.selection.body} isShort={false} />
       </div>
     );
   } else {
     return (
-      <>
+      <div className="no-paste-selected">
         <p>Select a paste</p>
-      </>
+      </div>
     );
   }
 }
--- src/utils/LineBreak.tsx
+++ src/utils/LineBreak.tsx
@@ -6,12 +6,12 @@ export function LineBreak(props: {
     ? props.str.split("\n").slice(0, 5)
     : props.str.split("\n");
   return (
-    <>
+    <div className={props.isShort ? "paste-body" : "selected-paste-body"}>
       {wordArr.map((line, idx) => (
         <div key={idx}>
           {line} <br />
         </div>
       ))}
-    </>
+    </div>
   );
 }
